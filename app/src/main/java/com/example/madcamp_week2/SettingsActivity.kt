c566d5d9b954e13b9bf28d20faf065844d01784a
2021059507 <2021059507@hanyang.ac.kr>
2023-07-10 21:47:24 +0900

    frag5

--- app/src/main/java/com/example/madcamp_week2/SettingsActivity.kt
+++ app/src/main/java/com/example/madcamp_week2/SettingsActivity.kt
@@ -2,9 +2,16 @@ package com.example.madcamp_week2
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.kakao.sdk.user.UserApiClient
 
 class SettingsActivity : AppCompatActivity() {
@@ -14,6 +21,25 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val rootView = layoutInflater.inflate(R.layout.activity_settings, null)
+
+        val crewSpinner = rootView.findViewById<Spinner>(R.id.spinner_settings_crew)
+        val crews = arrayOf("group1", "group2", "group3")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, crews)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        crewSpinner.adapter = adapter
+
+        crewSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedGroup = parent.getItemAtPosition(position).toString()
+                // 선택된 그룹을 처리하는 로직을 여기에 작성하세요.
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // 선택이 해제될 때의 처리 로직을 여기에 작성하세요.
+            }
+        }
 
         kakaoLogoutButton = findViewById(R.id.kakao_logout_button)
         kakaoUnlinkButton = findViewById(R.id.kakao_unlink_button)

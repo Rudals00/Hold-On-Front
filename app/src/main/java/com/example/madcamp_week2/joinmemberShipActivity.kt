095bf76cdf33aa566099ce062249c83dc105b62a
2019041703 <rudals9686@naver.com>
2023-07-11 02:50:12 +0900

    새벽3시

--- app/src/main/java/com/example/madcamp_week2/joinmemberShipActivity.kt
+++ app/src/main/java/com/example/madcamp_week2/joinmemberShipActivity.kt
@@ -3,15 +3,28 @@ package com.example.madcamp_week2
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import org.json.JSONObject
+import java.io.IOException
 
 
 class joinmemberShipActivity : AppCompatActivity() {
     private val PICK_IMAGE_REQUEST = 101
     private lateinit var addProfileImg : ImageView
+    private var imageUri: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_joinmembership)
@@ -25,7 +38,7 @@ class joinmemberShipActivity : AppCompatActivity() {
         }
         // 가입하기 버튼에 클릭 리스너 설정
         buttonJoin.setOnClickListener {
-            // SecondActivity를 시작하는 Intent 생성
+            signUp()
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent) // Intent를 사용하여 SecondActivity 시작
         }
@@ -34,11 +47,61 @@ class joinmemberShipActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
             val uri = data.data
+            imageUri = uri.toString()
             Glide.with(this)
                 .load(uri)
                 .into(addProfileImg)
             // Here you can use the uri to create a File object, open an input stream, etc.
             // ...
         }
+    }
+    private fun signUp() {
+        val client = OkHttpClient()
+
+        // 사용자 정보를 가져옵니다.
+        val nickname = findViewById<EditText>(R.id.memberNickname).text.toString()
+        val password = findViewById<EditText>(R.id.memberPassword).text.toString()
+        val email = findViewById<EditText>(R.id.memberEmail).text.toString()
+        val imageUrl = imageUri // 이미지의 URL을 어떻게 가져올지 결정해야 합니다.
+
+        // JSON 데이터를 생성합니다.
+        val json = JSONObject()
+        json.put("nickname", nickname)
+        json.put("password", password)
+        json.put("email", email)
+        json.put("imageUrl", imageUrl)
+
+        val mediaType = "application/json; charset=utf-8".toMediaType()
+        val body = json.toString().toRequestBody(mediaType)
+
+        // POST 요청을 생성합니다.
+        val request = Request.Builder()
+            .url("http://172.10.5.168/signup") // 서버의 주소를 입력해주세요.
+            .post(body)
+            .build()
+
+        // 요청을 비동기로 실행합니다.
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    // 메인 스레드에서 동작하는 코드 블록을 생성
+                    runOnUiThread {
+                        Toast.makeText(this@joinmemberShipActivity, "Signup successful!", Toast.LENGTH_LONG).show()
+                        val intent = Intent(this@joinmemberShipActivity, SecondActivity::class.java)
+                        startActivity(intent) // 가입 성공 후 SecondActivity 시작
+                    }
+                } else {
+                    runOnUiThread {
+                        Toast.makeText(this@joinmemberShipActivity, "Signup failed!", Toast.LENGTH_LONG).show()
+                    }
+                }
+                Log.d("SignUp", "Response: ${response.body?.string()}")
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                // TODO: 네트워크 요청이 실패했을 때 적절한 동작을 수행하세요.
+                Log.d("SignUp", "Failed to send request: ${e.localizedMessage}")
+            }
+        })
     }
 }
